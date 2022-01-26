439c6deeec37b1cd44b60bdde5ebc68c9d00c73a
TurnerEntenmann <32148366+TurnerEntenmann@users.noreply.github.com>
2022-01-26 12:32:04 -0600

    Rasa Component
    TODO: beef up examples, error handling, remove unnecessary components, ...

---
+++ rasaSearcher/actions/actions.py
@@ -0,0 +1,52 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+import os
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+from rasa_sdk.forms import FormAction
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
+
+class fsearch_action(Action):
+    def name(self) -> Text:
+        return "fsearch_action"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+            
+            name = tracker.get_slot("file_name")
+            command = "spSearcher f " + "target.txt"
+            os.system(command)
+            return []
+        
+class asearch_action(Action):
+    def name(self) -> Text:
+        return "asearch_action"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        os.system("spSearcher a google-chrome")
+                
+        return []
